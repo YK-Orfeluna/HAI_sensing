7b6d8bf37c39fb7a243cffa898b1b9eeac6e2838
YK-Orfeluna <k030913@kansai-u.ac.jp>
2017-04-05 20:26:48 +0900

    170405

--- slave/slave.ino
+++ slave/slave.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 
 const int HR = 1;        //Analog-pin(HR)
-const int GSR = 4;       //Analog-pin(GSR)
+const int GSR = 3;       //Analog-pin(GSR)
 const int LED = 13;
 
 const int THREAD = 500 ;
@@ -9,7 +9,7 @@ const int THREAD = 500 ;
 int hr = 0;         //sensor-value(HR)
 int gsr = 0;        //sensor-value(GSR)
 
-const byte H = 'h';
+const byte H = 'H';
 const byte G = 'G';
 byte hr1 = 0;
 byte hr2 = 0;
@@ -25,13 +25,8 @@ void setup() {
 }
 
 void loop() {
-  delay(10);
-}
-
-void sendByte(){
   hr = analogRead(HR);
   gsr = analogRead(GSR);
-
   if(hr > THREAD){
     digitalWrite(LED, HIGH);
   }
@@ -39,6 +34,10 @@ void sendByte(){
     digitalWrite(LED, LOW);
   }
 
+  delay(10);
+}
+
+void sendByte(){
   hr1 = lowByte(hr);
   hr2 = highByte(hr);
   gsr1 = lowByte(gsr);
