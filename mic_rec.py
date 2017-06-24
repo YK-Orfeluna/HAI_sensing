4a74fd2a88e58622803c700fd149bbd0e60a7621
YK-Orfeluna <k030913@kansai-u.ac.jp>
2017-06-24 12:29:58 +0900

    170624

--- mic_rec.py
+++ mic_rec.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*
 
+import sys, wave, time
 import pyaudio
-import wave, time
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16 # int16型
-CHANNELS = 2             # ステレオ
-RATE = 44100             # 441.kHz
+v = sys.version_info[0]
+if v == 2 :			# Pythonのバージョンに合わせたTkinterのimportを行う（このスクリプトでは2基準）
+	import Tkinter
+	import tkMessageBox
+	import tkFileDialog
+elif v == 3 :
+	import tkinter as Tkinter
+	from tkinter import messagebox as tkMessageBox
+	from tkinter import filedialog as tkFileDialog
+else :
+	exit("*This script only supports Python2.x or 3.x.\nSorry, we can not support your Python.")
+
+p = pyaudio.PyAudio()		# pyaudioの用意
+
+def get_device() :			# pyaudioが認識できるマイク類を取得する
+	global p
+
+	devices = []
+	for i in range(p.get_device_count()) :
+		devices.append(p.get_device_info_by_index(i))
+
+	names = []
+	for i in devices :
+		names.append(i["name"])
+
+	return names
+names = get_device()
 
 def rec_time() :
 	t = time.localtime()
@@ -27,14 +50,68 @@ def rec_time() :
 
 	return start, rec
 
+root = Tkinter.Tk()
+root.title("Select Device & Recording Time")
+root.geometry("500x500")
+
+#録音時間を設定する
+#editbox = Tkinter.Entry()			# 入力ボックス
+#editbox.pack()
+
+#録音終了時間を設定する
+label_time = Tkinter.Label(root, text="Finish Record Time")
+label_time.pack()
+
+box_hour = Tkinter.Entry(root, width=3)
+box_hour.insert(Tkinter.END, 12)		# 初期入力値を設定する
+box_hour.pack()
+
+label_hour = Tkinter.Label(root, text="h")
+label_hour.place(x=270, y=32)
+
+box_minute = Tkinter.Entry(root, width=3)
+box_minute.insert(Tkinter.END, 10)		# 初期入力値を設定する
+box_minute.pack()
+
+label_hour = Tkinter.Label(root, text="m")
+label_hour.place(x=270, y=60)
+
+# ラジオボタンを呼び出して，各ボタンに各デバイスを与える
+var = Tkinter.IntVar()				# 最終的に，ここに選んだラジオボタンの値が格納される
+for x, name in enumerate(names) :
+	button = Tkinter.Radiobutton(root, text=name, variable=var, value=x)
+	button.pack()
+
+# 録音開始のボタンを用意する．
+# root.quit()でroot.mainloop()の外＝その後のコード実行
+end_button = Tkinter.Button(root, text="Start Recoding", command=root.quit)
+end_button.pack()
+
+root.mainloop()
+
+# 入力/選択した値を取得する
+#sec = editbox.get()
+end_time = (int(box_hour.get()), int(box_minute.get()))
+index = var.get()			# 選んだラジオボタンに呼応した値
+
+
+# 録音設定
+CHUNK = 1024
+FORMAT = pyaudio.paInt16 # int16型
+CHANNELS = 2             # ステレオ
+RATE = 44100             # 441.kHz
 WAVE_OUTPUT_FILENAME, RECORD_SECONDS = rec_time()
 WAVE_OUTPUT_FILENAME += ".wav"
 
 #RECORD_SECONDS = 10
 
-p = pyaudio.PyAudio()
-
-stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+stream = p.open(	format=FORMAT, 
+					channels=CHANNELS, 
+					rate=RATE, 
+					input=True, 
+					frames_per_buffer=CHUNK, 
+					input_device_index=index,
+				)
 
 print("* recording")
 
@@ -43,7 +120,6 @@ frames = []
 start_time = time.time()
 time_flag = 1
 
-end_time = (12 , 10)
 while True :
 	data = stream.read(CHUNK)
 	frames.append(data)
